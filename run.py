b065b55a1ca133e8f4ec135915677ce8d6e1b3ea
Jaeseung Park <jaeseung.p@gmail.com>
2024-05-15 12:36:53 +0900

    Update run.py

--- run.py
+++ run.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
         sell_point = row["sell_point"]  # 매도 포인트
         buffer_buy_divide = row["buffer_buy_divide"] # 버퍼 매수 분할 수치
         buffer_sell_divide = row["buffer_sell_divide"] # 버퍼 매도 분할 수치
+        stop_loss_threshold = row["stop_loss_threshold"]  # 손절매 기준 예: 10%
         batch_yn = True
         
-        assessment_result = DoTrading(stock_name,balance_amount,start_trade_date,end_trade_date,buy_times,buffer_trading_point,sell_point,buffer_buy_divide,buffer_sell_divide,batch_yn)
+        assessment_result = DoTrading(stock_name,balance_amount,start_trade_date,end_trade_date,buy_times,buffer_trading_point,sell_point,buffer_buy_divide,buffer_sell_divide,stop_loss_threshold,batch_yn)
     
         print(f"Assessment result: {assessment_result}")
         
@@ -29,6 +30,8 @@ if __name__ == "__main__":
         
     # print(df['final_assessment'])
  
-    # 변경된 DataFrame을 다시 Excel 파일로 저장
-    df.to_excel('infinitTrading_result.xlsx', sheet_name='sheet2', index=False)
+    # 변경된 DataFrame을 sheet2에 저장
+    with pd.ExcelWriter('infinitTrading.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+        df.to_excel(writer, sheet_name='sheet2', index=False)
+
  
